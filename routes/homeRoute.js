53ffd8f87c63aaff245af447aa14f435808bad08
Jordan Devaney <jordandevaney28@gmail.com>
2019-09-21 23:08:52 -0400

    add weather info

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -11,7 +11,13 @@ route.get('/', (req, res) => {
     let days = week.getFullDate().dayOfWeek;
     let year = week.getFullDate().year;
     let date = week.getFullDate().date;
-    res.render('home', { days, month, date, year });
+
+    //get current weather from API
+    const openWeather ='64f62dc83f15692ea7febf3994776d4b';
+    request(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${openWeather}`, (err, response, body) => {
+    let weather =  JSON.parse(body);
+    res.render('home', { days, month, date, year, weather });
+  });
 });
 
 module.exports = route;
