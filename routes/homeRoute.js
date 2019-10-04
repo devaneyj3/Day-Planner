5943abe4a9a9df9865224d57f27a1669838656a1
Jordan Devaney <jordandevaney28@gmail.com>
2019-10-04 18:53:35 -0400

    commit

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,30 +1,27 @@
 //jshint esversion:6
 const express = require('express');
-const request = require('request');
 const route = express.Router();
 
 const week = require('../data/getCurrentWeek');
+const APIdata = require('../data/getWeather');
 
-let dateList = [];
+let dates = [];
+
+const apikey ='64f62dc83f15692ea7febf3994776d4b';
+let location = 'Whitmore Lake, US';
+let weatherURL =`https://api.openweathermap.org/data/2.5/weather?q=${location},uk&appid=${apikey}`;
 
 //render the main page
 route.get('/', (req, res) => {
-    
-    week.getFullDate(dateList);
-
-    //get current weather from API
-    //TODO: SEPERATE IN ANOTHER MODULE
-    const openWeather ='64f62dc83f15692ea7febf3994776d4b';
-    request(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${openWeather}`, (err, response, body) => {
-    let weather =  JSON.parse(body);
-    res.render('home', { dateList, weather });
-  });
+  week.getFullDate(dates);
+  APIdata.getData(weatherURL, res.render({ dates }));
+  
 });
 
 //get indivual page for the dates based on the variable "ID"
 route.get("/datePage/:id", ( req, res ) => {
   let date = req.params.id;
-  date = dateList[date]; //find the index of the date array based on the id variable exp: 0, 1, 2..
+  date = dates[date]; //find the index of the date array based on the id variable exp: 0, 1, 2..
 
   res.render("datePage", { date });
 });
