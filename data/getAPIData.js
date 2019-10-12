e1e1590ad00006d86a2281914f5e5a29bd85d9ab
Jordan Devaney <jordandevaney28@gmail.com>
2019-10-12 16:59:52 -0400

    ADD TODO COMMENT

--- data/getAPIData.js
+++ data/getAPIData.js
@@ -1,6 +1,9 @@
 //jshint esversion: 6
 const request = require('request');
 
+/*
+* TODO: MODULIZE API KEY AND MAKE LOCATION DYNAMIC
+*/
 const apikey ='64f62dc83f15692ea7febf3994776d4b';
 let location = 'Whitmore Lake, US';
 let url =`https://api.openweathermap.org/data/2.5/weather?q=${location},uk&appid=${apikey}`;
